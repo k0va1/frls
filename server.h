ca75dab9f9fc0eb372c318355d3d7e4c95c75b57
Alex Koval <al3xander.koval@gmail.com>
2024-01-18 12:41:08 +0100

    introduce Source structure

--- server.h
+++ server.h
@@ -7,8 +7,14 @@
 typedef enum { UNINITIALIZED, INITIALIZED, SHUTDOWN } SeverStatus;
 
 typedef struct {
+  char *file_path;
+  char *content;
+} Source;
+
+typedef struct {
   Config *config;
   ParsedInfo *parsed_info;
+  Source *sources;
   SeverStatus status;
   int server_socket;
   int client_socket;
@@ -16,6 +22,8 @@ typedef struct {
   struct sockaddr_in *client_address;
 } Server;
 
+void print_sources(Source *sources);
+void sync_source(Server *server, char *file_path, char *text);
 Server *create_server(Config *config);
 void destroy_server(Server *server);
 void accept_message(Server *server, char *buffer);
